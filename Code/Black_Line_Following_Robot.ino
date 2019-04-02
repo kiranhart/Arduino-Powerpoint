957ef2a2b7a21c137ab3d6f91b6d7ab3c307f674
Kiran Hart <kiran.hart@Live.com>
2019-04-02 10:19:57 -0400

    Updated code
    Updated the code for the line following robot, edited specifically for my robot's motors. Also added additional comments and left / right sensor checks.

--- Code/Black_Line_Following_Robot.ino
+++ Code/Black_Line_Following_Robot.ino
@@ -1,46 +1,45 @@
 /*
- * Code by Kiran Hart
- * https://www.GitHub.com/kiranhart/
- */
-
-
-/*
-Define the pins of which the motors are connected to,
-we'll make them constants since don't want these to be
-changed anywhere else in the program but here.
- */
-private const int LM1 = 2; // Left Motor #1
-private const int LM2 = 3; // Left Motor #2
-private const int RM1 = 4; // Right Motor #1
-private const int RM2 = 5; // Right Motor #2
+   Code by Kiran Hart
+   https://www.GitHub.com/kiranhart/
+*/
 
 /*
-Create variables to store which digital pin the IR
-sensors are connected to. (Which digital pin "R" 
-connects to from the QTI IR Sensor.
- */
-private int leftSensorPin = 6;
-private int rightSensorPin = 7;
+  Define the pins of which the motors are connected to,
+  we'll make them constants since don't want these to be
+  changed anywhere else in the program but here.
+*/
+const int LM1 = 9; // Left Motor #1
+const int LM2 = 10; // Left Motor #2
+const int RM1 = 11; // Right Motor #1
+const int RM2 = 12; // Right Motor #2
+
+/*
+  Create variables to store which digital pin the IR
+  sensors are connected to. (Which digital pin "R"
+  connects to from the QTI IR Sensor.
+*/
+int leftSensorPin = 2;
+int rightSensorPin = 3;
 
 void setup() {
   //Start the serial monitor so we can print
   //any important data to be viewed by the user.
   Serial.begin(9600);
-  
+
   //Setup the pinModes for the motors
   pinMode(LM1, OUTPUT);
   pinMode(LM2, OUTPUT);
   pinMode(RM1, OUTPUT);
-  pinMode(RM2, OUTPUT); 
+  pinMode(RM2, OUTPUT);
 }
 
 /*
- * This method will be used to charge and discharge the capacitor on the IR sensor, doing
- * this will allow us to read either a zero or one. If it reads zero that means that it's 
- * detecting a white surface, if it reads a one then that means it's detecting a black surface.
- */
+   This method will be used to charge and discharge the capacitor on the IR sensor, doing
+   this will allow us to read either a zero or one. If it reads zero that means that it's
+   detecting a white surface, if it reads a one then that means it's detecting a black surface.
+*/
 int getIRSensorValue(int sensorPin) {
-  //Charge the capacitor 
+  //Charge the capacitor
   pinMode(sensorPin, OUTPUT);
   digitalWrite(sensorPin, HIGH);
   delay(1);
@@ -52,9 +51,17 @@ int getIRSensorValue(int sensorPin) {
 }
 
 /*
- * This will be used to control the motors, each motor has two values, depending on the values
- * either high or low (0 -255) the speed can be changed, same for the rotation direction.
- */
+   This will be used to control the motors, each motor has two values, depending on the values
+   either high or low (0 -255) the speed can be changed, same for the rotation direction.
+
+   In Our case: 
+   Left/Right Speed One > 0 && Left/Right Speed Two == 0 will move the motors in the correct direction
+   If your's is moving backwards you can switch the two wire pins for each motor, for example, connect
+   the wire that goes to pin 3 of the L293D to pin 6, and 6 to 3. Same for the other motor if needed.
+
+   If you don't want to rewire setting the left/right speed two > 0 and left/right speed one == 0 may work.
+   
+*/
 void moveMotors(int leftSpeedOne, int leftSpeedTwo, int rightSpeedOne, int rightSpeedTwo) {
   analogWrite(LM1, leftSpeedOne);
   analogWrite(LM2, leftSpeedTwo);
@@ -65,22 +72,22 @@ void moveMotors(int leftSpeedOne, int leftSpeedTwo, int rightSpeedOne, int right
 void loop() {
   //Both of the sensors are detecting a white surface.
   if (getIRSensorValue(leftSensorPin) == 0 && getIRSensorValue(rightSensorPin) == 0) {
-    //Move the motors forward, at a constant speed 
-    moveMotors(HIGH, LOW, HIGH, LOW);
-  } 
-
-  //Left sensor is detecting a black surface, Right sensor is detecting a white surface
-  if (getIRSensorValue(leftSensorPin) == 1 && getIRSensorValue(rightSensorPin) == 0) {
-    
-  }
-
-  //Left sensor is detecting a white surface, Right sensor is detecting a black surface
-  if (getIRSensorValue(leftSensorPin) == 0 && getIRSensorValue(rightSensorPin) == 1) {
-    
-  }
-
-  //Both of the sensors are detecting a black surface, stop the robot.
-  if (getIRSensorValue(leftSensorPin) == 1 && getIRSensorValue(rightSensorPin) == 1) {
-    moveMotors(LOW, LOW, LOW, LOW);
+    /*Move the motors forward, at a constant speed
+    The Right motor is slowed down to 90 because the left one in this case
+    is much slower at full speed compared to the right, causing it to drive at an angle. But if
+    both motors move at the same speed at 255, set it to 255*/
+    moveMotors(255, 0, 90, 0);
+  } else if (getIRSensorValue(leftSensorPin) == 1 && getIRSensorValue(rightSensorPin) == 0) {
+    /*If the left sensor is detecting a black surface / line slow down the left wheel so that 
+    the right one will be moving faster allowing the robot to turn left.*/
+    moveMotors(90, 0, 90, 0);
+  } else if (getIRSensorValue(leftSensorPin) == 0 && getIRSensorValue(rightSensorPin) == 1) {
+    /*If the right sensor is detecting a black surface / line slow down the right wheel so 
+    the left one will be moving faster causing the robot to turn right*/
+    moveMotors(255, 0, 40, 0);
+  } else if (getIRSensorValue(leftSensorPin) == 1 && getIRSensorValue(rightSensorPin) == 1) {
+    /*In this case if both of the IR sensors are detecting a black surface / line then we should
+    just stop the robot from moving any more than it needs to.*/
+    moveMotors(0, 0, 0, 0);
   }
 }
